713720c9ce22e474900c34b9e578581b4d5df5b3
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-04-30 09:48:08 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
-// Listen for messages from content script
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === "blockWebsite") {
-    chrome.tabs.update(sender.tab.id, { url: chrome.extension.getURL("blocked.html") });
+// Receive message from content script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === "blockWebsite") {
+    // Get the URL of the blocked page
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const blockedUrl = tabs[0].url;
+      // Open blocked.html with the blocked URL as a query parameter
+      chrome.tabs.create({ url: "blocked.html?blockedUrl=" + encodeURIComponent(blockedUrl) });
+    });
   }
 });
